56b358e818ddb7bd3ea37d7565b40d83807f69f7
Marcos Maceo <marcosmaceowork@gmail.com>
2019-03-21 14:52:03 -0400

    [skyapi] refs #4 Update sudo in gradle_install

--- travis/gradle_install.sh
+++ travis/gradle_install.sh
@@ -7,4 +7,4 @@ touch /etc/profile.d/gradle.sh
 echo "export GRADLE_HOME=/opt/gradle/gradle-5.0" >> /etc/profile.d/gradle.sh
 echo "export PATH=${GRADLE_HOME}/bin:${PATH}" >> /etc/profile.d/gradle.sh
 sudo chmod +x /etc/profile.d/gradle.sh
-source /etc/profile.d/gradle.sh
+sudo source /etc/profile.d/gradle.sh
