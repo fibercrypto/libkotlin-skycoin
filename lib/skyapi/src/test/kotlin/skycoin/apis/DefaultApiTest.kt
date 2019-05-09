f669f3f01a04aa362879aeaec1c37365d65d8a96
stdevAdrianPaez <a.paez@estudiantes.matcom.uh.cu>
2019-05-09 13:35:21 -0400

    [skyapi] refs #4 Add test for API

---
+++ lib/skyapi/src/test/kotlin/skycoin/apis/DefaultApiTest.kt
@@ -0,0 +1,11 @@
+package skycoin.apis
+
+import skycoin.junit.Assert.assertEquals
+import skycoin.junit.Test
+
+class MyLibraryTest{
+    @Test fun testAPI(){
+        assertEquals("API","API")
+        assertEquals("API","AIP")
+    }
+}
